8c44420025efaf1a7e2bbb738d5b3a4338887557
abrehan2 <rehannajam2@gmail.com>
2024-03-10 18:42:48 +0500

    Chore: Added an initial calling point for app.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv')
 const http = require('http')
 const app = require('./app')
 const connectDatabase = require('./config/database')
+const logger = require('./utils/winston')
 const { initializeSocket } = require('./utils/location')
-const logger = require("./utils/winston")
 
 // CONFIG -
 if (process.env.NODE_ENV !== 'PRODUCTION') {
@@ -18,7 +18,7 @@ initializeSocket(server)
 // HANDLING UNCAUGHT EXCEPTION -
 process.on('uncaughtException', (err) => {
   console.log(`ERROR: ${err.message}`)
-  logger("error", err.message)
+  logger('error', err.message)
   process.exit(1)
 })
 
@@ -32,7 +32,7 @@ const runner = server.listen(process.env.PORT, () => {
 
 // UNHANDLED PROMISE REJECTION -
 process.on('unhandledRejection', (err) => {
-  console.log(`ERROR: ${err.message}`)  
+  console.log(`ERROR: ${err.message}`)
   logger('error', err.message)
   runner.close(() => process.exit(1))
 })
---
+++ server/start/routes.js
@@ -0,0 +1,55 @@
+// IMPORTS -
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const errorMiddleware = require('../middlewares/error')
+const cors = require('cors')
+const morgan = require('morgan')
+const path = require('path')
+
+// STARTUP FUNCTION -
+module.exports = (app) => {
+  // MIDDLEWARES -
+  app.use(express.json())
+  app.use(cookieParser())
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.use(morgan('dev'))
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+  app.use(fileUpload())
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.BASE_URL,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      optionsSuccessStatus: 204,
+    }),
+  )
+
+  // FOR TESTING -
+  app.get('/', (_req, res) => {
+    return res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  })
+
+  // IMPORT ROUTES -
+  const userRoute = require('../routes/userRoute')
+  const bloodBankRoute = require('../routes/bloodBankRoute')
+  const bloodGroupRoute = require('../routes/bloodGroupRoute')
+  const bloodRequestRoute = require('../routes/bloodRequestRoute')
+  const bloodDonationRoute = require('../routes/bloodDonationRoute')
+  const eventRoute = require('../routes/eventRoute')
+  const placeRequestRoute = require('../routes/placeRequestRoute')
+
+  // ROUTES -
+  app.use('/api', userRoute)
+  app.use('/api', bloodBankRoute)
+  app.use('/api', bloodGroupRoute)
+  app.use('/api', bloodRequestRoute)
+  app.use('/api', bloodDonationRoute)
+  app.use('/api', eventRoute)
+  app.use('/api', placeRequestRoute)
+
+  // MIDDLEWARE FOR ERROR-HANDLING -
+  app.use(errorMiddleware)
+}
