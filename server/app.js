0b6463210df02305d17786bb8970aef794212d0e
Vyro MacBook <vyromacbook@192.168.1.8>
2024-03-14 00:36:44 +0500

    Chore: Implemented a few APIs

--- server/app.js
+++ server/app.js
@@ -3,13 +3,14 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 
-// CALLING THE STARTUP FUNCTION -
-require('./start/routes')(app)
-
 // CONFIG -
 if (process.env.NODE_ENV !== 'PRODUCTION') {
   dotenv.config({ path: './config/config.env' })
 }
+
+// CALLING THE STARTUP FUNCTION -
+require('./start/routes')(app)
+
 
 // // FOR DEPLOYMENT -
 // app.get("/", (req, res) => {
