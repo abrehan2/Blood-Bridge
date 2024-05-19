13b7a154cdcfdc245e560514e8283b1809be2e91
abrehan2 <rehannajam2@gmail.com>
2024-05-19 18:49:37 +0500

    Chore: Modified an API

--- server/controllers/placeRequestController.js
+++ server/controllers/placeRequestController.js
@@ -106,7 +106,7 @@ const saveRecord = async (req, bloodBanks) => {
 
 // GET RECORDS -
 exports.getRecords = catchAsyncErr(async (req, res) => {
-  const records = await saveRequestModel.find().populate("bloodBanks", "name");
+  const records = await saveRequestModel.find().populate("bloodBanks Requester", "name name");
 
   res.status(200).json({
     success: true,
